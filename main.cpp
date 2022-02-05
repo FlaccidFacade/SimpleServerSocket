dab72acdb1f6a57c851c7018c07483b579513967
FlaccidFacade <zachary.glenn.turner@gmail.com>
2022-02-05 08:55:44 -0500

    Simplify

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // Server side C/C++ program to demonstrate Socket programming
 // This is only set to work with IPV4 and TCP
 // Build and run. go to browser and type localhost:8080 to see message
+// review socket.h for function usage
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -30,48 +31,27 @@ int main(int argc, char const *argv[])
 
     
     */
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
        
     // Set socket options.
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,&opt, sizeof(opt));
 
-    
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-
+    //create address struct
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
        
     // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
 
+    // prepare for connections on socket
+    listen(server_fd, 3);
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
+    // wait for connection and open a new socket when it arrives
+    new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
 
     //send hello world message
+    //sends N bytes of the "hello" buffer
     send(new_socket , hello , strlen(hello) , 0 );
    
     return 0;
