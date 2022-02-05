1c7f23dc2f555d86f621c427d486fb52b07ba1b7
FlaccidFacade <zachary.glenn.turner@gmail.com>
2022-02-04 22:09:41 -0500

    update documentation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 // Server side C/C++ program to demonstrate Socket programming
+// This is only set to work with IPV4 and TCP
+// Build and run. go to browser and type localhost:8080 to see message
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -13,22 +15,37 @@ int main(int argc, char const *argv[])
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    char *hello = "Hello from server";
+    char *hello = "Hello World";
        
     // Creating socket file descriptor
+    /*int sockfd = socket(domain, type, protocol)
+    
+    domain:
+        AF_LOCAL - also known as AF_UNIX used to same host operations
+        AF_INET - used with IPV4
+            ---in this case we can use LOCALHOST:PORT#
+    type:
+        SOCK_STREAM - TCP
+        SOCK_DGRAM - UDP
+
+    
+    */
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
        
-    // Forcefully attaching socket to the port 8080
+    // Set socket options.
+
+    
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                                                   &opt, sizeof(opt)))
     {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
@@ -45,15 +62,17 @@ int main(int argc, char const *argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
+
+
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
                        (socklen_t*)&addrlen))<0)
     {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
+
+    //send hello world message
     send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+   
     return 0;
 }
